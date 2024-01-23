c3920dc686dd54e1a02038d490eb9aa3b463b3f5
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2024-01-23 10:56:08 +0800

    .

--- phrase_name_and_height.c
+++ phrase_name_and_height.c
@@ -2,9 +2,66 @@
 #include <wchar.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
 #include <locale.h> //你如果不加這個，就會出現亂碼(???)
 /*C99 standard (ISO/IEC 9899:1999):
 7.11.1.1 The setlocale function (p: 205-206)*/
+
+typedef struct Node
+{
+    void *data;
+    struct Node *next;
+} Node;
+
+typedef struct Linkedlist
+{
+    Node *Head;
+    Node *Tail;
+} Linkedlist;
+
+typedef struct Heap{
+    //TODO ING!
+}
+
+void initLinkedlist(Linkedlist *list)
+{
+    list->Head = NULL;
+    list->Tail = NULL;
+}
+
+void addHead(Linkedlist *list, void *data)
+{
+    Node *tmp = (Node *)malloc(sizeof(Node));
+    if (list->Tail == NULL)
+    {
+        list->Tail = tmp;
+        tmp->next = NULL;
+    }
+    else
+    {
+        tmp->next = list->Head;
+    }
+    list->Head = tmp;
+}
+
+typedef struct Member{
+int age;
+int height;
+char name[];
+}Member;
+
+void addNewMember(Linkedlist *list, Member *member){
+addHead(list,(void*)member);
+}
+
+void addNewMember_via_name_age_height(char* name,int age,int height){
+Member *member=(Member*)malloc(sizeof(Member)+strlen(name)+1);//!IMPORTANT!我都忘記這裡這樣搞要加一
+member->age=age;
+member->height=height;
+strcpy(member->name,name);
+}
+
 int main()
 {
     // Set the locale to support wide characters
@@ -43,7 +100,33 @@ int main()
             ab = true;
             printf("\x1b[31;49m"); // 紅色
         }
-        wprintf(L"%ls", buffer);
+        // if string include "漢語拼音：", \x1b[30;43m
+        if (wcsstr(buffer, L"漢語拼音：") != NULL) // TODO? if not wchar_t ?
+        {
+            printf("\x1b[30;43m"); // 黑底黃字
+            // buffer.foreach
+            wchar_t *p = buffer;
+            while (*p != L'\0')
+            {
+                if (*p == L'：')
+                {
+                    printf("\x1b[34;43;1;3;4m");
+                    wprintf(L"%lc", *p);
+                    printf("\x1b[0m"); // 白色
+                }
+                else
+                {
+                    printf("\x1b[30;43m");
+                    wprintf(L"%lc", *p);
+                    printf("\x1b[0m"); // 白色
+                }
+                p++;
+            }
+        }
+        else
+        {
+            wprintf(L"%ls", buffer);
+        }
     }
 
     // Close the file
